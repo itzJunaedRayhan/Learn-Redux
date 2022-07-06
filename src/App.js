966031a93170fe3f133641b9457b4645761d2755
itzJunaedRayhan <webgenesis99@gmail.com>
2022-07-06 20:13:21 +0600

    Learned Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Count from './Components/Count';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import Control from './Components/Control';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="wrapper">
+          <h2>Hello</h2>
+          <Count/>
+          <Control/>
+      </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
